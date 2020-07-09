975f02513233d96f1832ba2a105e5e9781a83ab7
RGaldamez <ricardo.j.galdamez@gmail.com>
2020-07-09 13:37:12 -0600

    Adding new ShowScreen to show blog details

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import BlogScreen from './src/screens/BlogScreen';
 import {Provider} from './src/context/BlogContext';
-
+import BlogScreen from './src/screens/BlogScreen';
+import ShowScreen from './src/screens/ShowScreen';
 const navigator = createStackNavigator(
   {
     Blogs: BlogScreen,
+    Show: ShowScreen,
   },
 
   {
